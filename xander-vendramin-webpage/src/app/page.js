c56d916ab746d5748a69bf328442307a255acd41
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-04-08 16:18:34 -0600

    Initial Commit

---
+++ xander-vendramin-webpage/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <title>Xander Vendramin Webpage</title>
+      <header className="min-h-10 items-center justify-items-center bg-cyan-600">
+        <p className=""> Xander Vendramin Webpage</p>
+      </header>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-sans)] bg-cyan-700">
+      <p className="">Hello World!</p>
+      </div>
+    </div>
+  );
+}
