3616e565513b865bf9f1b15191b3f9e84f44e43d
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-06-02 16:12:41 -0600

    website changes

--- xander-vendramin-webpage/src/app/contact/page.js
+++ xander-vendramin-webpage/src/app/contact/page.js
@@ -10,8 +10,9 @@ export default function Home() {
       <div className="flex flex-1 items-center justify-center sm:items-stretch">
         <div className="hidden sm:ml-6 sm:block">
           <div className="flex space-x-4">
-            <a href="\" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-            <a href="/projects" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a> 
+            <a href="\" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+            <a href="/projects" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+            
           </div>
         </div>
       </div>
@@ -19,16 +20,17 @@ export default function Home() {
   </div>
   <div className="sm:hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pt-2 pb-3">
-      <a href="\" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
+      <a href="\" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
       <a href="/projects" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+      
     </div>
   </div>
 </nav>
       <div className="font-[family-name:var(--font-sans)] text-white bg-gradient-to-bl from-slate-900 to-gray-950 min-h-screen">
         <div className="justify-items-center place-content-center ">
-          <h1 className="text-shadow-lg text-5xl mt-10">Portfolio of Xander Vendramin</h1>
+          <h1 className="text-shadow-lg text-4xl mt-10">Portfolio of Xander Vendramin</h1>
       </div>
-        <div className="w-2/3 justify-self-center p-5 mt-10 border-6 border-solid shadow-lg text-xl bg-slate-700 border-slate-800">
+        <div className="w-2/3 justify-self-center p-5 mt-10 border-6 border-solid shadow-lg bg-slate-700 border-slate-800">
           <p>
           I am a new graduate from the University of Regina with a bachelor's degree majoring in 
           computer science. I am looking for an opportunity to use my coding skills and strong foundational
@@ -39,8 +41,7 @@ export default function Home() {
           <p>
             Contact Info: <br></br> Email: xvendramin@gmail.com <br></br> Phone: +1-306-552-4547 
             <br></br> LinkedIn: <a className="text-blue-500" href="https://www.linkedin.com/in/xander-vendramin/" target="_blank"><u>Click Here</u></a>
-            <br></br> GitHub: <a className="text-blue-500" href="https://github.com/xander-vendramin" target="_blank"><u>Click Here</u></a>
-            <br></br> Resume: <a className="text-blue-500" href="Xander Vendramin Resume.pdf" download><u>Click Here (Download)</u></a>
+            <br></br> Resume: <a className="text-blue-500" href="Xander Vendramin Resume.pdf" download><u>Click Here</u></a>
           </p>
         </div>
       </div>
---
+++ xander-vendramin-webpage/src/app/projects/page.js
@@ -0,0 +1,79 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  //Navbar outline provided by tailwind open source code
+  <div>
+  <nav className="bg-slate-700">
+  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="relative flex h-16 items-center justify-between">
+      <div className="flex flex-1 items-center justify-center sm:items-stretch">
+        <div className="hidden sm:ml-6 sm:block">
+          <div className="flex space-x-4">
+            <a href="\" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+            <a href="/projects" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Projects</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="sm:hidden" id="mobile-menu">
+    <div className="space-y-1 px-2 pt-2 pb-3">
+      <a href="\" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
+      <a href="/projects" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Projects</a>  
+    </div>
+  </div>
+</nav>
+      <div className="font-[family-name:var(--font-sans)] text-white bg-gradient-to-bl from-slate-900 to-gray-950 min-h-screen">
+      <h1 className="text-shadow-lg text-5xl justify-items-center place-self-center p-10">Projects</h1>
+      <div className="grid grid-rows-3 lg:grid-cols-3 max-h-500 lg:max-h-300 gap-3 p-6 ml-20 mr-20">
+        <div className="">
+          <div className="p-2 border-6 border-solid shadow-lg bg-slate-700 border-slate-800">
+          <p className="text-shadow-lg text-2xl mb-3 place-self-center"><u>Unity VR Showcase</u></p>
+          <iframe className="justify-self-center border-solid rounded-b-md mb-3" height="300" width="100%" src="https://www.youtube.com/embed/hw_NRuF31b4" allowFullScreen></iframe>
+            <p>Stack Used: Unity, C#, OVR</p>
+            <br></br>
+            <p>This is a University project that I worked on for a class I took about developing software for Virtual Reality. I worked on the project
+            by myself within a timeframe of about one month. I was responsible for everything in the project except the actual
+            creation of models and assets, which were all free open source assets. This project was my first time using Unity and
+            C# for a project of this scale, and I was specifically proud of how I managed to take on a new programming language
+            and work environment while creating a final project I was very happy with.
+            </p>
+          </div>
+        </div>
+        <div className="">
+          <div className="p-2 border-6 border-solid shadow-lg bg-slate-700 border-slate-800">
+            <p className="place-self-center mb-3"><a href="https://film-finder-publish.vercel.app/" className="text-shadow-lg text-2xl text-blue-500"  target="_blank"><u>Film Finder - Click to View</u></a></p>
+            <p>Stack Used: React, NextJS, ChakraUI, MongoDB</p>
+            <br></br>
+            <p>Film Finder is a website I worked on in University on with a team of five people. We had a limited timeline
+              to create a fully functioning full stack website for our final project. My main contribution to this project was
+              the back end. I designed account creation and adjustments, login validation, and account storage in a MongoDB database. I
+              also worked on the website's ability to fetch movie reviews, rating, and other information on the page. I also
+              contributed some front end work on pages such as the user profile page, user reviews page, friend list page, and a 
+              few other adjustments across other pages. <br></br> I decided to take the code for this website and adjust it so
+              it could be hosted on Vercel. As of now, all of the functionality I worked on is able to be used on the website,
+              but I am working on trying to get our Python integration to work on the hosted website.
+            </p>
+          </div>
+        </div>
+        <div className="">
+        <div className="p-2 border-6 border-solid shadow-lg bg-slate-700 border-slate-800">
+          <p className="text-shadow-lg text-2xl mb-3 place-self-center"><u>Unity Level Generation</u></p>
+          <iframe className="justify-self-center border-solid rounded-b-md mb-3" height="300" width="100%" src="https://www.youtube.com/embed/FU0A3x0K6k8" allowFullScreen></iframe>
+          <p>Stack Used: Unity, C#</p>
+            <br></br>
+            <p>This is a demo of some of my recent independent game development work. This specific showcase is meant to 
+              display the functionality of level generation in my game, which utilizes some of the more complex code I've written
+              for this project. The level generation works by placing rooms of interest in a grid, then I use some math (Delauny
+              Triangulation) to plot hallways from room to room, and then premade assets are placed in the game
+              space for both hallways and rooms of interest. I was proud of my ability to learn and understand a new way to solve a complicated problem 
+              and implement it effectively and modularly into this project.
+            </p>
+        </div>
+      </div>
+      </div>
+      </div>
+    </div>
+  );
+}
