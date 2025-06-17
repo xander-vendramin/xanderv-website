113c0e7fac2782d87833fc45924c0d3933105dca
xander-vendramin <158534289+xander-vendramin@users.noreply.github.com>
2025-06-16 18:27:54 -0600

    fix again

--- xander-vendramin-webpage/src/app/page.js
+++ xander-vendramin-webpage/src/app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
   </div>
   <div className="sm:hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pb-3">
-      <a href="\" className="text-center place-self-center block w-1/3 rounded-md bg-gray-900 px-3 py-2 font-medium font-[family-name:var(--font-oswald)] text-white" aria-current="page">Home</a>
-      <a href="/projects" className="text-center place-self-center block w-1/3 rounded-md px-3 py-2 font-medium font-[family-name:var(--font-oswald)] text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+      <a href="\" className="text-center place-self-center md:place-self-center block w-1/3 rounded-md bg-gray-900 px-3 py-2 font-medium font-[family-name:var(--font-oswald)] text-white" aria-current="page">Home</a>
+      <a href="/projects" className="text-center place-self-center md:place-self-center block w-1/3 rounded-md px-3 py-2 font-medium font-[family-name:var(--font-oswald)] text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
     </div>
   </div>
 </nav>
